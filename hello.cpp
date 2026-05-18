e8609175fb4900046e4b98d6041bd32a3dfb22e4
Zinaa-4673 <zinaa1038762@my.yosemite.edu>
2026-05-18 07:05:17 -0700

    Add files via upload

--- hello.cpp
+++ hello.cpp
@@ -1,1 +1,11 @@
+// CSCI 27X - GitHub Upload Practice
+// Name: Zinaa
+// Section: 271
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello from GitHub! My first commit works." << endl;
+    return 0;
+}
